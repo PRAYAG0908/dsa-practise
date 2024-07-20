ba2f937deacd590d068de327ddba5dd983f9236b
Prayag <prayag.pac@gmail.com>
2024-07-20 21:35:09 +0530

    linear search

---
+++ Searching_algos/linear_search.py
@@ -0,0 +1,13 @@
+#linear or sequential search. Here we basically search for an element in an array.
+
+def linearsearch(A,key):
+    index = 0
+    while index < len(A):
+        if A[index] == key:
+            return "Element Found at index :", index
+        index = index+1
+    return "Element Not Found!"
+
+A = [21,27,45,87,63]
+element = linearsearch(A,45)
+print(element)
