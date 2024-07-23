2f090adf38a069d86ab7e8d59428eaa98524f1f9
Prayag <prayag.pac@gmail.com>
2024-07-23 21:30:51 +0530

    selection sort

--- Sorting_algos/selection_sort.py
+++ Sorting_algos/selection_sort.py
@@ -1,1 +1,19 @@
-#This is sorting technique
+#This is a Comparison Based Sorting Algorithm. Sorting is a process of rearranging the elements, the goal is to rearrange the elements in sorted order, either ascending or descending order. Time Complexity for Rearranging O(n^2) [n(n-1)/2] and for swapping is A[n]
+
+def selectionsort(A):
+    n = len(A)
+    for i in range(n-1):
+        position = i
+        for j in range(i+1,n):
+            if A[j] < A[position]:
+                position = j
+
+        temp = A[i]
+        A[i] = A[position]
+        A[position] = temp
+
+A = [5,34,65,9,20]
+print("Original Array:",A)
+
+selectionsort(A)
+print("Sorted Array:",A)
